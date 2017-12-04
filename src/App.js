7528372c6e78d836ae2e402424c9103dfc41361d
Colleen Lavin <cml388@mun.ca>
2017-12-04 01:57:00 -0600

    added pictures. reverted to css out of frustration

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import Facebook from './Facebook.svg'
 import Instagram from './Instagram.svg'
-import './App.scss';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -41,41 +41,41 @@ class App extends Component {
               <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
             </div>
             <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/Pb6bYbB.png" alt="Katie |" />
+              <h3 className="item-label"> <span className="bold"> Katie | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/Tzo7BPJ.pngg" alt="Katie | colors" />
             </div>
             <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/6dcDKJ0.png" alt="Cindy |" />
+              <h3 className="item-label"> <span className="bold"> Cindy | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/aXbYa8r.png" alt="Cindy | colors" />
             </div>
             <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/LmfA7Hh.png" alt="Jill |" />
+              <h3 className="item-label"> <span className="bold"> Jill | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/XKhtx6w.png" alt="Jill | colors" />
             </div>
           </div>
           <div className="row">
           <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/yAor5Wg.png" alt="Holly |" />
+              <h3 className="item-label"> <span className="bold"> Holly | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/UapXGoq.png" alt="Holly | colors" />
             </div>
             <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/eGCioqI.png" alt="Terri |" />
+              <h3 className="item-label"> <span className="bold"> Terri | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/xjNVGmz.png" alt="Terri | colors" />
             </div>
             <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/1P03p93.png" alt="Arielle |" />
+              <h3 className="item-label"> <span className="bold"> Arielle | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/4ah9vF7.png" alt="Arielle | colors" />
             </div>
             <div className="col-md-3 col-lg-3 col-sm-6">
-              <img className='img-item' src="https://i.imgur.com/YCvItd9.png" alt="Beth |" />
-              <h3 className="item-label"> <span className="bold"> Beth | </span> <span className="selected-color">L124578 - tan </span></h3>
-              <img className='item-colors' src="https://i.imgur.com/v9LoT3e.png" alt="Beth | colors" />
+              <img className='img-item' src="https://i.imgur.com/6cBlyid.png" alt="Ally |" />
+              <h3 className="item-label"> <span className="bold"> Ally | </span> <span className="selected-color">L124578 - tan </span></h3>
+              <img className='item-colors' src="https://i.imgur.com/3XrArk6.png" alt="Ally | colors" />
             </div>
           </div>
         </div>
